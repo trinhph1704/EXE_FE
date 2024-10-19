290972e7748f28fa0127bfae83b11dde0f021380
HungHy1602 <leconghung160202@gmail.com>
2024-10-19 13:43:47 +0700

    Update App.jsx

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import ListItemPage from './pages/UserPage/ListItemPage/ListItemPage';
+import ItemDetails from './Components/User/ItemDetails/ItemDetails';
+import NotFound from './pages/NotFound/NotFound';
+import ProfilePage from './pages/UserPage/ProfilePage/ProfilePage';
+import CartPage from './pages/UserPage/CartPage/CartPage';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        {/* Trang Login sẽ hiển thị khi truy cập root ("/") */}
+        <Route path="/Login" element={<Login />} />
+
+        {/* Trang Home sẽ hiển thị khi truy cập "/home" */}
+        <Route path="/Home" element={<Home />} />
+
+        {/* Trang danh sách sản phẩm */}
+        <Route path="/Product" element={<ListItemPage />} />
+
+        {/* Trang chi tiết sản phẩm */}
+        <Route path="/Product/Details/:title" element={<ItemDetails />} />
+
+        {/* Trang NotFound sẽ hiển thị cho mọi route không xác định */}
+        <Route path="*" element={<NotFound />} /> {/* Đây là route cho trang NotFound */}
+
+        {/* Trang chỉ thông tin người dùng */}
+        <Route path="/Profile" element={<ProfilePage />} />
+
+        {/* Trang chỉ qua trang Thanh Toán và Giỏ Hàng  */}
+        <Route path="/Cart" element={<CartPage />} />
+        {/* comment */}
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
