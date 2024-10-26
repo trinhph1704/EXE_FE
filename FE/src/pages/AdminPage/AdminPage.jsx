c501d7043da2ff32ebdb5f3d7ba04259a7707ca7
trinhph1704 <trinhphse170414@fpt.edu.vn>
2024-10-26 17:05:25 +0700

    update dashboard sidebard

--- FE/src/pages/AdminPage/AdminPage.jsx
+++ FE/src/pages/AdminPage/AdminPage.jsx
@@ -1,27 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./../AdminPage/AdminPage.css";
 import Dashboard from "../../Components/Admin/Dashboard/Dashboard";
 import ProductList from "../../Components/Admin/ProductList/ProductList";
 import AccountList from "../../Components/Admin/AccountList/AccountList";
+import Sidebar from "../../Components/Admin/Sidebar/Sidebar";
 
 export default function AdminPage() {
+    const [activeTab, setActiveTab] = useState("Dashboard");
     return (
         <div id="AdminPage">
             <div className="AdminPage-Container">
-                <h2>
-                    Dash Board
-                    <Dashboard />
-                </h2>
-
-                <h2>
-                    Product List
-                    <ProductList />
-                </h2>
-                <h2>
-                    Account List
-                    <AccountList />
-                </h2>
+                <Sidebar/>
+            <div className="Sidebar-Dashboard">
+            <Sidebar setActiveTab={setActiveTab} activeTab={activeTab} />
+                    <div className="Content">
+                        {activeTab === "Dashboard" && <Dashboard />}
+                        {activeTab === "Account" && <AccountList />}
+                        {activeTab === "Item" && <ProductList />}
+                    </div>
+                    </div>
 
             </div>
         </div>
